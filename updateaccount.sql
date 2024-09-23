3889a4f326d424bc8ac88c266a4e3eea4b6aac41
Rianna2002 <2303254@sit.singaporetech.edu.sg>
2024-09-23 19:32:54 +0800

    Revert "im still in confusion, will fixfix another time"
    This reverts commit c6dc2057b97e7537d2b31409ba4113a2266fcde5.

--- updateaccount.sql
+++
@@ -1,6 +0,0 @@
-UPDATE User
-SET FirstName = @FirstName, 
-    LastName = @LastName,
-    Email = @Email,
-    Password = @Password -- Ensure to hash the password before storing
-WHERE UserId = @UserId;
